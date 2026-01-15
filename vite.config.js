35e06fb77ac07ae88021dc2fdca19a3fcf1354b2
Lynn_HomeWin <lynnhu97@163.com>
2026-01-16 02:09:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { skillEditorPlugin } from './plugins/skillEditorPlugin.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ArkLines/',  // GitHub Pages 部署路径
+    base: '/',  // GitHub Pages 部署路径
     plugins: [
         react(),
         skillEditorPlugin()  // 技能编辑器 API 插件
